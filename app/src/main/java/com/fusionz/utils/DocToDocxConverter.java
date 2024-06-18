2b2b33df1bea3cdac24c5ee86f859240165c24fc
coolvm9 <coolvm9@gmail.com>
2024-06-18 15:27:49 -0700

    updates

--- app/src/main/java/com/fusionz/utils/DocToDocxConverter.java
+++ app/src/main/java/com/fusionz/utils/DocToDocxConverter.java
@@ -1,8 +1,6 @@
 package com.fusionz.utils;
-
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.converter.WordToHtmlConverter;
-import org.apache.poi.hwpf.usermodel.Range;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.jsoup.Jsoup;
@@ -15,10 +13,6 @@ import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSSerializer;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -39,16 +33,16 @@ public class DocToDocxConverter {
         LSSerializer serializer = implLS.createLSSerializer();
         String htmlContent = serializer.writeToString(htmlDocument);
 
-        // Step 2: Manipulate HTML with JSoup (optional)
-        Document jsoupDoc = Jsoup.parse(htmlContent);
-        // Example manipulation: Add a new paragraph
-        jsoupDoc.body().appendElement("p").text("This is a new paragraph added via JSoup.");
-        String manipulatedHtml = jsoupDoc.html();
+        // Write HTML to file
+        String htmlFilePath = file.getAbsolutePath().replace(".doc", ".html");
+        try (FileOutputStream htmlOut = new FileOutputStream(htmlFilePath)) {
+            htmlOut.write(htmlContent.getBytes());
+        }
 
-        // Step 3: Convert HTML to .docx
+        // Step 2: Convert HTML to .docx
         XWPFDocument docx = new XWPFDocument();
-        Document manipulatedJsoupDoc = Jsoup.parse(manipulatedHtml);
-        Elements paragraphs = manipulatedJsoupDoc.body().select("p");
+        Document jsoupDoc = Jsoup.parse(htmlContent);
+        Elements paragraphs = jsoupDoc.body().select("p");
         for (Element paragraph : paragraphs) {
             XWPFParagraph docxParagraph = docx.createParagraph();
             docxParagraph.createRun().setText(paragraph.text());
